089c8ebeee806b638245c1e78f4d1e3e0e5a44f2
Mateus Koppe <mateus.koppe@gmail.com>
2017-11-23 01:44:12 -0200

    feat(style): add group-button

--- src/app/components/lc-ui/lc-ui.module.js
+++ src/app/components/lc-ui/lc-ui.module.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import { LuiInputComponent } from './lui-input/lui-input.component';
 import { LuiListComponent } from './lui-list/lui-list.component';
 import { LuiFloatButtonComponent } from './lui-float-button/lui-float-button.component';
+import { LuiAlertComponent } from './lui-alert/lui-alert.component';
 
 export const LcUiModule = angular
   .module('lcUiModule', [
@@ -10,4 +11,5 @@ export const LcUiModule = angular
   .component('luiInput', LuiInputComponent)
   .component('luiList', LuiListComponent)
   .component('luiFloatButton', LuiFloatButtonComponent)
+  .component('luiAlert', LuiAlertComponent)
   .name;
