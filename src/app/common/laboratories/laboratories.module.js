8d1c1996301d168801640e2e2db896875132d972
Mateus Koppe <mateus.koppe@gmail.com>
2017-11-27 16:14:52 -0200

    feat(laboratories): create route laboratories

--- src/app/common/laboratories/laboratories.module.js
+++ src/app/common/laboratories/laboratories.module.js
@@ -4,6 +4,7 @@ import uiRouter from '@uirouter/angularjs';
 import { ToolsModule } from './tools/tools.module';
 import { LaboratoriesListComponent } from './laboratories-list/laboratories-list.component';
 import { LaboratoryComponent } from './laboratory/laboratory.component';
+import { LaboratoriesComponent } from './laboratories.component';
 import { LaboratoriesService } from './laboratories.service';
 
 export const LaboratoriesModule = angular
@@ -14,12 +15,17 @@ export const LaboratoriesModule = angular
   .service('LaboratoriesService', LaboratoriesService)
   .component('laboratoriesList', LaboratoriesListComponent)
   .component('laboratory', LaboratoryComponent)
+  .component('laboratories', LaboratoriesComponent)
   .config(($stateProvider, routeCheckLogin) => {
     'ngInject';
     $stateProvider
       .state('laboratory', {
         url: '/laboratory/{id}',
         component: 'laboratory',
+      })
+      .state('laboratories', {
+        url: '/laboratories',
+        component: 'laboratories',
       })
   })
   .name;
