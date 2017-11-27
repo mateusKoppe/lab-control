613008654debc52ac55266910f961f7e0d92336e
Mateus Koppe <mateus.koppe@gmail.com>
2017-11-27 20:39:13 -0200

    refactor(lc-ui): only load element on open

--- src/app/components/lc-ui/lui-alert/lui-alert.component.js
+++ src/app/components/lc-ui/lui-alert/lui-alert.component.js
@@ -8,41 +8,63 @@ export const LuiAlertComponent = {
   transclude: true,
   templateUrl,
   controller: class LuiAlertController {
-    constructor($document, $scope){
+    constructor($document, $scope, $timeout){
       'ngInject';
-      this._document = $document;
-      this._scope = $scope;
+      this.$document = $document;
+      this.$scope = $scope;
+      this.$timeout = $timeout;
     }
 
     $onInit(){
       this._showing = this.open;
-      this._scope.$watch('$ctrl.open', value => {
-        this.showing = value;
+      this.$scope.$watch('$ctrl.open', value => {
+        this.toggleAlert();
       });
-      this._listenEscKey();
+      this.printElement = this.open;
     }
 
     set showing(showing){
       this._showing = showing;
       this.open = showing;
-      this._refreshClass();
     }
 
     get showing(){
       return this._showing;
     }
 
-    close(){
+    toggleAlert(){
+      if(this.open){
+        this.openAlert();
+      } else {
+        this.closeAlert();
+      }
+    }
+
+    openAlert(){
+      this.printElement = true;
+      this.showing = true;
+      this.$timeout(() => this._refreshClass(), 10);
+      this._listenEscKey();
+    }
+
+    closeAlert(){
       this.showing = false;
+      this._refreshClass();
+      this.$timeout(() => this.printElement = false, 300);
+      this._unlistenEscKey();
     }
 
     _listenEscKey(){
-      this._document.bind('keydown', event => {
+      this.$document.bind('keydown', event => {
         if(event.keyCode == 27){
           this.showing = false;
-          this._scope.$apply();
+          this.$scope.$apply();
         }
       });
+    }
+
+    _unlistenEscKey(){
+      this.$document.unbind('keydown');
     }
 
     _refreshClass(){
