fdbe1a7c92bb97e55151e0013523403c0fb21001
Mateus Koppe <mateus.koppe@gmail.com>
2018-08-30 19:20:26 -0300

    refactor(assets): change style imports

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import { CommonModule } from './common/common.module';
 import { ComponentsModule } from './components/components.module';
-import './styles/styles.scss';
+import './assets/scss/styles.scss';
 
 import { AppComponent } from './app.component';
 import './app.component.scss';
